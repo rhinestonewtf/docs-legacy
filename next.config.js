76a874b12d760bf8be58832e30bc82475a88c86f
kopy-kat <kopp.konrad@gmail.com>
2025-01-23 16:31:20 +0000

    feat: add accents

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/omniaccount',
+        destination: '/omni-account',
         permanent: true,
       },
     ]
