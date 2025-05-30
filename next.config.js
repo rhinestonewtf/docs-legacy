58a6246d42105d64e58a9eb55f465782d6040bf7
kopy-kat <kopp.konrad@gmail.com>
2025-05-30 12:13:21 +0100

    feat: revert to old docs structure

--- next.config.js
+++ next.config.js
@@ -9,13 +9,4 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/omni-account',
-        permanent: false,
-      },
-    ]
-  },
 }
